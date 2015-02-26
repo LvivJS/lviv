8bca6606acd6e810ceb42e68831b73b3192aabb1
anastasia22 <abuniak22@gmail.com>
2015-02-26 14:49:37 +0200

    add styles for header block

--- app/scripts/modules/Header.js
+++ app/scripts/modules/Header.js
@@ -4,14 +4,14 @@ var React = require('react');
 var Header = React.createClass({
 	render: function() {
 		return(
-			<div id="header">
-				<div id="logo"><img src="./images/logo.jpg"/></div>
-				<div id="menu">
+			<div id="header" className="header">
+				<div id="logo" className="header__logo"><img src="./images/logo.jpg"/></div>
+				<div id="menu" className="menu">
 					<ul>
-						<li><a href="#">Register</a></li>
-						<li><a href="#">Schedule</a></li>
-						<li><a href="#">Reporters</a></li>
-						<li><a href="#">Locating</a></li>
+						<li className="menu__item"><a href="#">Register</a></li>
+						<li className="menu__item menu__item--active"><a href="#">Schedule</a></li>
+						<li className="menu__item"><a href="#">Reporters</a></li>
+						<li className="menu__item"><a href="#">Locating</a></li>
 					</ul>
 				</div>
 			</div>
