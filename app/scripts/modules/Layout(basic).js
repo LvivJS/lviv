ad450f32fe7e067cfc881c0276a695e0f9811333
Andrii Pylat <pilatao91@gmail.com>
2015-02-27 17:44:08 +0200

    #cm_7 and  #cm_11. Added a basic layout and google map module.

---
+++ app/scripts/modules/Layout(basic).js
@@ -0,0 +1,57 @@
+'use strict';
+
+var React = require('react');
+var Header = require('./Header');
+var Menu = require('./Menu');
+var LocationMap = require('./LocationMap');
+
+var LayoutBasic = React.createClass({
+	render: function() {
+		return (
+			<div className="page-wrap">
+				<div id="header">
+
+					<Header />
+
+				</div>
+				<div id="menu">
+
+					<Menu />
+
+				</div>
+				<div id="overview">
+					<div className="container">
+						here to be inserted overview module
+					</div>
+				</div>
+				<div id="speakers">
+					<div className="container">
+						here to be inserted speakers module
+					</div>
+				</div>
+				<div id="shedule">
+					<div className="container">
+						here to be inserted shedule module
+					</div>
+				</div>
+				<div id="location">
+					
+					<LocationMap />
+					
+				</div>
+				<div id="registration">
+					<div className="container">
+						here to be inserted registration module
+					</div>
+				</div>
+				<div id="footer">
+					<div className="container">
+						here to be inserted footer module
+					</div>
+				</div>
+			</div>
+		);
+	}
+});
+
+module.exports = LayoutBasic;
