93e511fbafb9dc4518b38d82a10a7c559487704a
Andrii Pylat <pilatao91@gmail.com>
2015-03-02 11:29:59 +0200

    menu text color and markup improvement.

--- app/scripts/modules/Menu.js
+++ app/scripts/modules/Menu.js
@@ -3,15 +3,13 @@ var React = require('react');
 var Menu = React.createClass({
   render: function() {
     return (
-      <div className="menu">
-        <ul>
-          <li className="menu__item"><a href="#overview">Overview</a></li>
-          <li className="menu__item"><a href="#speakers">Speakers</a></li>
-          <li className="menu__item"><a href="#shedule">Shedule</a></li>
-          <li className="menu__item"><a href="#location">Location</a></li>
-          <li className="menu__item"><a href="#registration">Registration</a></li>
-        </ul>
-      </div>
+      <nav className="menu">
+          <a href="#overview" className="menu__item">Overview</a>
+          <a href="#speakers" className="menu__item">Speakers</a>
+          <a href="#shedule" className="menu__item">Shedule</a>
+          <a href="#location" className="menu__item">Location</a>
+          <a href="#registration" className="menu__item">Registration</a>
+      </nav>
     );
   }
 });
