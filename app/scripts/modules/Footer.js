a801fb3e710e7b9367b0d4de7215441d6a23cda5
Andrii Pylat <pilatao91@gmail.com>
2015-03-02 16:40:57 +0200

    markup and styles for footer

--- app/scripts/modules/Footer.js
+++ app/scripts/modules/Footer.js
@@ -6,21 +6,51 @@ var Footer = React.createClass({
 	render: function() {
     return (
       <div className="footer">
-      	<div className="footer--partners-wrap">
-      		<h3 className="footer--partners-header">Golden</h3>
-      		<div className="footer--partners-items">
+      	<div className="footer__partners-wrap">
+      		<h3 className="footer__partners-header">Golden</h3>
+      		<div className="footer__partners-items">
+      			<a href="#" tittle="partner" className="footer__partner-link"></a>
+      			<a href="#" tittle="partner" className="footer__partner-link"></a>
+      			<a href="#" tittle="partner" className="footer__partner-link"></a>
+      			<a href="#" tittle="partner" className="footer__partner-link"></a>
+      			<a href="#" tittle="partner" className="footer__partner-link"></a>
       		</div>
       	</div>
-      	<div className="footer--partners-wrap">
-      		<h3 className="footer--partners-header">Silwer</h3>
-      		<div className="footer--partners-items">      			
+      	<div className="footer__partners-wrap">
+      		<h3 className="footer__partners-header">Silver</h3>
+      		<div className="footer__partners-items"> 
+	      		<a href="#" tittle="partner" className="footer__partner-link"></a>
+	      		<a href="#" tittle="partner" className="footer__partner-link"></a>
+	      		<a href="#" tittle="partner" className="footer__partner-link"></a>
+	      		<a href="#" tittle="partner" className="footer__partner-link"></a>
+	      		<a href="#" tittle="partner" className="footer__partner-link"></a>     			
       		</div>
       	</div>
-      	<div className="footer--partners-wrap">
-      		<h3 className="footer--partners-header">Bronze</h3>
-      		<div className="footer--partners-items">      			
+      	<div className="footer__partners-wrap">
+      		<h3 className="footer__partners-header">Bronze</h3>
+      		<div className="footer__partners-items">  
+      			<a href="#" tittle="partner" className="footer__partner-link"></a>
+      			<a href="#" tittle="partner" className="footer__partner-link"></a>
+      			<a href="#" tittle="partner" className="footer__partner-link"></a>
+      			<a href="#" tittle="partner" className="footer__partner-link"></a>
+      			<a href="#" tittle="partner" className="footer__partner-link"></a>
+      			<a href="#" tittle="partner" className="footer__partner-link"></a>
+      			<a href="#" tittle="partner" className="footer__partner-link"></a>
+      			<a href="#" tittle="partner" className="footer__partner-link"></a>
+      			<a href="#" tittle="partner" className="footer__partner-link"></a>
+      			<a href="#" tittle="partner" className="footer__partner-link"></a>    			
       		</div>
       	</div>
+      	<div className="footer__partners-wrap">
+      		<h3 className="footer__organizers-header">Organizers</h3>
+      		<div className="footer__partners-items">  
+      			<a href="#" tittle="partner" className="footer__partner-link"></a>
+      			<a href="#" tittle="partner" className="footer__partner-link"></a>
+      			<a href="#" tittle="partner" className="footer__partner-link"></a>
+      			<a href="#" tittle="partner" className="footer__partner-link"></a>		
+      		</div>
+      	</div>
+
       </div>
     );
   }
