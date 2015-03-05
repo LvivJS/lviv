9e4e9e3be1912d9de9deb2c704da4951d95a794b
Andrii Pylat <pilatao91@gmail.com>
2015-03-05 17:37:00 +0200

    added brakepoint mixin, added toggle button and functionality on it for bp(medium), styled menu for bp(medium). added some sass variables.

--- app/scripts/modules/Menu.js
+++ app/scripts/modules/Menu.js
@@ -51,11 +51,66 @@ window.addEventListener('scroll', function() {
     menuStyle.position = 'fixed';
     menuStyle.width = '100%';
     menuStyle.top = '0';
+<<<<<<< HEAD
+=======
+    menuStyle.zIndex = '1';
+>>>>>>> added brakepoint mixin, added toggle button and functionality on it for bp(medium), styled menu for bp(medium). added some sass variables.
     overview.style.paddingTop = menu.offsetHeight + 'px';
   } else {
     menuStyle.position = 'static';
     overview.style.paddingTop = '0px';
   }
 });
+
+//toggle label state and toggle menu visibility on sass bp(medium);
+window.onload = function menuToggle() {
+  var label = document.getElementById('menuToggle');
+  var checkBox =  document.getElementById('menuToggleBox');
+  var menuItems = document.getElementsByClassName('menu__item');
+  var menuButtonStripes = document.getElementsByClassName('menuToggle__stripe');
+  //label events
+
+  function setStripeMargin() {
+    menuButtonStripes[1].style.marginTop = '4.5px';
+    menuButtonStripes[1].style.marginBottom = '4.5px';
+  }
+
+  function nullStripeMargin() {
+    menuButtonStripes[1].style.marginTop = '0px';
+    menuButtonStripes[1].style.marginBottom = '0px';
+  }
+  //make stripes on label closer together
+  label.onmouseout = nullStripeMargin;
+
+  //make stripes on label further apart
+  label.onmouseover = setStripeMargin;
+
+  var toggleDisplay = function(v) {
+    for (i = 0; i < menuItems.length; i++) {
+      menuItems[i].style.display = v;
+      //let on click on item togle visibility of menu
+      menuItems[i].onclick = function() {
+        toggle();
+        checkBox.checked = false;
+        nullStripeMargin();
+      };
+    }
+  };
+
+  function toggle() {
+    if (!checkBox.checked) {
+      toggleDisplay('flex');
+      setStripeMargin('4.5px');
+      label.onmouseout = setStripeMargin;
+    } else {
+      toggleDisplay('none');
+      label.onmouseout = nullStripeMargin;
+    }
+  }
+
+  label.onclick = function() {
+    toggle();
+  }
+};
 
 module.exports = Menu;
