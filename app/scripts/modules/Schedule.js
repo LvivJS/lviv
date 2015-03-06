1ab7dc68e6926238706fde00c83f1b302bf038fd
anastasia22 <abuniak22@gmail.com>
2015-03-06 18:14:15 +0200

    improved styles

--- app/scripts/modules/Schedule.js
+++ app/scripts/modules/Schedule.js
@@ -29,23 +29,22 @@ var Schedule = React.createClass({
 });
 
 var Conference = React.createClass({
-  getInitialState:function() {
+  getInitialState: function() {
     return {
       activeTable:this.props.days[0],
       activeDay:this.props.days[0].day_id,
-      isVisible:true
+      confIsVisible:true
     }
   },
-  ChangeTab:function(day) {
+  ChangeTab: function(day) {
     this.setState({
       activeTable:day,
       activeDay: day.day_id
     });
 
   },
-  ChangeConfRepresent:function() {
-    this.state.isVisible ? this.setState({isVisible:false}) : this.setState({isVisible:true});
-
+  ChangeConfRepresent: function() {
+    this.state.confIsVisible ? this.setState({confIsVisible:false}) : this.setState({confIsVisible:true});
   },
   // RendConference:function() {
   //   var days = this.props.days.map(function(day) {
@@ -56,7 +55,7 @@ var Conference = React.createClass({
   //       return <Timetable sessions={day.timetable} key={day.day_id}/>
   //     };
   //   }.bind(this));
-  //     if (this.state.isVisible) {
+  //     if (this.state.confIsVisible) {
   //       return (
   //     } else {return }
   //   }.bind(this),
@@ -65,16 +64,16 @@ var Conference = React.createClass({
       return <li onClick={this.ChangeTab.bind(null, day)} key={day.day_id} className={(this.state.activeDay==day.day_id)?"conference__tab--active":null}><span>{day.day_name}</span></li>
     }.bind(this));
     var timetable = this.props.days.map(function(day) {
-      if (day.day_id == this.state.activeDay && this.state.isVisible) {
+      if (day.day_id == this.state.activeDay && this.state.confIsVisible) {
         return <Timetable sessions={day.timetable} key={day.day_id}/>
       };}.bind(this));
     return (
       <div className="conference">
         <div className="conference__title">
           <h3>Shedule: {this.props.name}</h3>
-          <input type="button" onClick={this.ChangeConfRepresent} className={this.state.isVisible ? "up-arrow" : "down-arrow"}/>
+          <input type="button" onClick={this.ChangeConfRepresent} className={this.state.confIsVisible ? "up-arrow" : "down-arrow"}/>
         </div>
-        {this.state.isVisible ? <ul>{days}</ul> : null}
+        {this.state.confIsVisible ? <ul>{days}</ul> : null}
         {timetable}
       </div>
     );
@@ -82,27 +81,58 @@ var Conference = React.createClass({
 });
 
 var Timetable = React.createClass({
+  getInitialState:function() {
+    return {
+      sessions:this.props.sessions
+    }
+  },
+  // changeSession: function(newSession) {
+  //   var newSessions = this.state.sessions.map(function(oneSession) {
+  //     if (oneSession.article == newSession.article) {
+  //       oneSession = newSession;
+  //     }
+  //     return oneSession
+  //   }.bind(this));
+  //   console.log(newSessions);
+  //   this.setState({sessions:newSessions})
+  // },
   render: function() {
-    var sessions = this.props.sessions.map(function(session) {
-      return (
-        <div className="session" key={session.article}>
-        <div className="session__time">{session.time}</div>
-          <div className="session__arrangement">
-            <h4 className="session__arrangement--name">{session.article}</h4>
-            <div className="session__arrangement--speaker">
-              <span className="speaker__name">{session.speaker.name}</span>
-              <span className="speaker__pos">{session.speaker.position}</span>
-            </div>
-            <div className="session__arrangement--about">{session.about}</div>
-          </div>
-        </div>
-      )
-    });
+    var sessions = this.state.sessions.map(function(session) {
+      return <Session key={session.article} session={session} /*onChange={this.changeSession}*//>
+    }.bind(this));
     return (
       <div className="timetable">
         {sessions}
       </div>
     );
+  }
+});
+
+var Session = React.createClass({
+  getInitialState: function() {
+    return {
+      session:this.props.session,
+      hidedContent:null
+    }
+  },
+  ChangeAbout: function(current) {
+    
+  },
+  render: function() {
+    return (
+      <div className="session" key={this.state.session.article}>
+        <div className="session__time">{this.state.session.time}</div>
+        <div className="session__arrangement">
+          <h4 className="session__arrangement--name">{this.state.session.article}</h4>
+          <div className="session__arrangement--speaker">
+            <span className="speaker__name">{this.state.session.speaker.name}</span>
+            <span className="speaker__pos">{this.state.session.speaker.position}</span>
+          </div>
+          <input type="button" onClick={this.ChangeAbout.bind(null,this.state.session)}/>
+          <div className="session__arrangement--about">{this.state.session.about}</div>
+        </div>
+      </div>
+    )
   }
 });
 
