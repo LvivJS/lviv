496b05e6b10fe83fe95f538c88a60ff85707e510
yasya <id68337466@gmail.com>
2015-03-11 18:18:49 +0200

    very simple form validation.

--- app/scripts/modules/Registration.js
+++ app/scripts/modules/Registration.js
@@ -1,25 +1,82 @@
 var React = require('react');
 
 var Registration = React.createClass({
-   propTypes: {
-    firstName: React.PropTypes.string.isRequired
+  getInitialState: function() {
+    return {
+      submitted: null
+    }
+  },
+  handleSubmit: function() {
+    if (this.refs.registration.isValid()) {
+      this.setState({submitted: this.refs.registration.getFormData()})
+    };
+    console.log(this.refs.registration.isValid());
   },
   render: function() {
+    return (<div className="registration">
+      <RegistrationForm ref="registration"/>
+      <div className="registration__field">
+          <input type="submit" onClick={this.handleSubmit} className="registration__submit" value="Register"/>
+      </div>
+    </div>
+    )
+  }
+});
+
+var RegistrationForm = React.createClass({
+  getInitialState: function() {
+    return {
+      errors: null
+    }
+  },
+  isValid: function() {
+    var fields = ['userName', 'email', 'password'];
+    var errors = {};
+    fields.forEach(function(field) {
+      var value = this.refs[field].getDOMNode().value
+      if (!value) {
+        errors[field] = 'This field is required';
+      }
+      // var nextEl = this.refs[field].getDOMNode().nextElementSibling;
+      // nextEl.className = "registration--error";
+    }.bind(this));
+    this.setState({errors: errors});
+
+    var isValid = true
+    for (var error in errors) {
+      isValid = false
+      break
+    }
+    return isValid
+  },
+   getFormData: function() {
+    var data = {
+      usertName: this.refs.userName.getDOMNode().value,
+      email: this.refs.email.getDOMNode().value,
+      password: this.refs.password.getDOMNode().value
+    }
+    return data
+  },
+  render: function() {
+    console.log(this.state.errors);
     return (
-      <form className="registration" role="form" onChange={this.handleChange}>
-        <div className="registration__fields">
-            <input className="registration__input registration__input--transition" type="text" value={this.props.firstName} ref="firstName" placeholder="User Name"/>
+      <div>
+        <div className="registration__field">
+            <input className="registration__input registration__input--transition" type="text" ref="userName" placeholder="User Name"/>
         </div>
-        <div className="registration__fields">
-            <input className="registration__input registration__input--transition" type="text" name="email" ref="email" placeholder="Email Address"/>
+
+        <span className="registration--error">this field it required</span>
+        <div className="registration__field">
+            <input className="registration__input registration__input--transition" type="text"  ref="email" placeholder="Email Address"/>
         </div>
-        <div className="registration__fields">
-            <input className="registration__input registration__input--transition" type="password" name="password" ref="password" value="" placeholder="Password"/>
+
+        <span className="registration--error">this field it required</span>
+        <div className="registration__field">
+            <input className="registration__input registration__input--transition" type="password"  ref="password" placeholder="Password"/>
         </div>
-        <div className="registration__fields">
-          <input type="submit" className="registration_submit" value="Register"/>
-        </div>
-      </form>
+
+        <span className="registration--error">this field it required</span>
+      </div>
     );
   }
 });
