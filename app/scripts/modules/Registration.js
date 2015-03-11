7ee30f67a2622278050b395264d48e725c8c27e2
yasya <id68337466@gmail.com>
2015-03-11 14:22:16 +0200

    fix again

--- app/scripts/modules/Registration.js
+++ app/scripts/modules/Registration.js
@@ -1,20 +1,23 @@
 var React = require('react');
 
 var Registration = React.createClass({
+   propTypes: {
+    firstName: React.PropTypes.string.isRequired
+  },
   render: function() {
     return (
       <form className="registration" role="form" onChange={this.handleChange}>
-        <div>
-            <input className="registration__input registration__input--transition" type="text" name="fName" ref="firstName" placeholder="User Name"/>
+        <div className="registration__fields">
+            <input className="registration__input registration__input--transition" type="text" value={this.props.firstName} ref="firstName" placeholder="User Name"/>
         </div>
-        <div>
+        <div className="registration__fields">
             <input className="registration__input registration__input--transition" type="text" name="email" ref="email" placeholder="Email Address"/>
         </div>
-        <div>
+        <div className="registration__fields">
             <input className="registration__input registration__input--transition" type="password" name="password" ref="password" value="" placeholder="Password"/>
         </div>
-        <div className="submit">
-          <input type="submit" value="Register"/>
+        <div className="registration__fields">
+          <input type="submit" className="registration_submit" value="Register"/>
         </div>
       </form>
     );
