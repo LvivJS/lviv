a79608f54fce0cec5142bd6a5c56b906bbec18b7
anastasia22 <abuniak22@gmail.com>
2015-03-13 17:48:49 +0200

    created basic responsive styles, validation, registration module

--- app/scripts/modules/Registration.js
+++ app/scripts/modules/Registration.js
@@ -1,9 +1,11 @@
 var React = require('react');
+var Firebase = require('firebase');
 
 var Registration = React.createClass({
   pushData:function(data) {
-    // function that pushes data to database
-    console.log(data)
+    var ref = new Firebase('https://blistering-fire-6843.firebaseio.com/users');
+    ref.push(data);
+    alert('You succesfully registered!');
   },
   render: function() {
     return (
@@ -19,40 +21,67 @@ var RegistrationForm = React.createClass({
     return ({
       name:false,
       email:false,
-      password:false
+      phone:false,
+      nameTipIsShown:false,
+      emailTipIsShown:false,
+      phoneTipIsShown:false,
+      clear:false
     })
   },
   handleSubmit: function(e) {
     e.preventDefault();
+    this.checkData();
     if (this.isValid()) {
-      this.props.onDataReceived(this.isValid())
-      console.log(this.isValid());
+      this.props.onDataReceived(this.isValid());
+      this.clearForm();
+    };
+  },
+  checkData: function() {
+    if (!this.state.name) {
+      this.setState({nameTipIsShown: true});
+    };
+    if (!this.state.email) {
+      this.setState({emailTipIsShown: true});
+    };
+    if (!this.state.phone) {
+      this.setState({phoneTipIsShown: true});
     };
   },
   nameIsValid: function(name) {
-    this.setState(name);
+    if (name) {
+      this.setState({name:name, nameTipIsShown: false});
+    };
   },
   emailIsValid: function(email) {
-    this.setState(email);
+    if (email) {
+      this.setState({email:email, emailTipIsShown: false});
+    };
   },
-  passwordIsValid: function(password) {
-    this.setState(password);
+  phoneIsValid: function(phone) {
+    if (phone) {
+      this.setState({phone: phone, phoneTipIsShown: false});
+    };
   },
   isValid: function() {
-    if (this.state.name && this.state.email && this.state.password) {
+    if (this.state.name && this.state.email && this.state.phone) {
       return {
         name: this.state.name,
         email: this.state.email,
-        password : this.state.password
+        phone : this.state.phone
       }
-    } else {return false}
+    } else {
+      return false
+    };
+  },
+  clearForm: function() {
+    this.setState({clear:true});
   },
   render: function() {
     return (
       <form onSubmit={this.handleSubmit}>
-        <NameInput valueReceived={this.nameIsValid}/>
-        <EmailInput valueReceived={this.emailIsValid}/>
-        <PasswordInput valueReceived={this.passwordIsValid}/>
+        <NameInput valueReceived={this.nameIsValid} tipIsShown={this.state.nameTipIsShown} clear={this.state.clear}/>
+        <EmailInput valueReceived={this.emailIsValid} tipIsShown={this.state.emailTipIsShown} clear={this.state.clear}/>
+        <PhoneInput valueReceived={this.phoneIsValid} tipIsShown={this.state.phoneTipIsShown} clear={this.state.clear}/>
         <div className="registration__field">
           <input type="submit" className="registration__submit" value="Register"/>
         </div>
@@ -64,72 +93,118 @@ var RegistrationForm = React.createClass({
 var NameInput = React.createClass({
   getInitialState: function() {
     return {
-      value: false
+      name:  true
     }
   },
   handleChange: function() {
+    var regExp = /^[a-zA-Z_ -]{3,50}$/;
     var value = this.refs.userName.getDOMNode().value;
-    if (value == 'pill') {
-      this.setState({
-        name: value
-      });
-      this.props.valueReceived({name:value})
+    if (regExp.test(value)) {
+      this.setState({name: value});
+      this.props.valueReceived(value)
+    } else {
+      this.setState({name: false});
+      this.props.valueReceived(false);
     }
   },
+  componentWillReceiveProps:function(props) {
+    if (props.clear) {
+      this.refs.userName.getDOMNode().value = '';
+    };
+  },
   render: function() {
+    var inputClass = 'registration__input ';
+    if (!this.state.name || this.props.tipIsShown) {
+      inputClass += 'registration__input--invalid'
+    };
+    var spanClass = 'registration__tip ';
+    if (!this.props.tipIsShown) {
+      spanClass += 'invisible'
+    }
     return (
       <div className="registration__field">
-        <input onChange={this.handleChange} className="registration__input registration__input--transition" type="text" ref="userName" placeholder="User Name"/>
-        <span className="registration--error">this field it required</span>
+        <input onChange={this.handleChange} className={inputClass} type="text" ref="userName" placeholder="User Name"/>
+        <span className={spanClass}>Name should have at least 3 characters, but no more than 16</span>
       </div>
     )
   }
 });
+
 var EmailInput = React.createClass({
   getInitialState: function() {
     return {
-      value: false
+      email: true
     }
   },
   handleChange: function() {
+    var regExp = /\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*([,;]\s*\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*)*/;
     var value = this.refs.email.getDOMNode().value;
-    if (value == 'hello') {
-      this.setState({
-        email: value
-      });
-      this.props.valueReceived({email:value})
+    if (regExp.test(value)) {
+      this.setState({email: value});
+      this.props.valueReceived(value)
+    } else {
+      this.setState({email: false});
+      this.props.valueReceived(false);
     }
   },
+  componentWillReceiveProps:function(props) {
+    if (props.clear) {
+      this.refs.email.getDOMNode().value = '';
+    };
+  },
   render: function() {
+    var inputClass = 'registration__input ';
+    if (!this.state.email || this.props.tipIsShown) {
+      inputClass += 'registration__input--invalid'
+    };
+    var spanClass = 'registration__tip ';
+    if (!this.props.tipIsShown) {
+      spanClass += 'invisible'
+    }
     return (
       <div className="registration__field">
-        <input onChange={this.handleChange} className="registration__input registration__input--transition" type="text"  ref="email" placeholder="Email Address"/>
-        <span className="registration--error">this field it required</span>
+        <input onChange={this.handleChange} className={inputClass} type="text"  ref="email" placeholder="Email Address"/>
+        <span className={spanClass}>Please, enter valid email</span>
       </div>
     )
   }
 });
-var PasswordInput = React.createClass({
+
+var PhoneInput = React.createClass({
   getInitialState: function() {
     return {
-      value: false
+      phone: true
     }
   },
   handleChange: function() {
-    var value = this.refs.password.getDOMNode().value;
-    if (value == '123') {
-      this.setState({
-        password: value
-      });
-      this.props.valueReceived({password:value})
+    var regExp = /^([0-9\(\)\/\+ \-]*)$/;
+    var value = this.refs.phone.getDOMNode().value;
+    if (regExp.test(value)) {
+      this.setState({phone:value});
+      this.props.valueReceived(value)
+    } else {
+      this.setState({phone: false});
+      this.props.valueReceived(false);
     }
-
+  },
+  componentWillReceiveProps:function(props) {
+    if (props.clear) {
+      this.refs.phone.getDOMNode().value = '';
+    }
   },
   render: function() {
+    var inputClass = 'registration__input ';
+    if (!this.state.phone || this.props.tipIsShown) {
+      inputClass += 'registration__input--invalid'
+    };
+    var spanClass = 'registration__tip ';
+    if (!this.props.tipIsShown) {
+      spanClass += 'invisible'
+    }
     return (
       <div className="registration__field">
-        <input onChange={this.handleChange} className="registration__input registration__input--transition" type="password"  ref="password" placeholder="Password"/>
-        <span className="registration--error">this field it required</span>
+        <input onChange={this.handleChange} className={inputClass} type="phone"  ref="phone" placeholder="Phone"/>
+        <span className={spanClass}>Phone must have at least 4 numeric digit.</span>
       </div>
     )
   }
