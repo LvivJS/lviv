2d626124fe3437a3d8a2015e0525effcff1fbac4
anastasia22 <abuniak22@gmail.com>
2015-03-17 17:37:44 +0200

    basic module and styles for overview are created

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -5,7 +5,8 @@ var config = (function() {
     path: {
       schedule: './dev/json/schedule.json',
       speakers: './dev/json/speakers.json',
-      partners: './dev/json/partners.json'
+      partners: './dev/json/partners.json',
+      mainInfo: './dev/json/mainInfo.json'
     },
     firebasePath: 'https://blistering-fire-6843.firebaseio.com/users'
   }
