28ddad1617a1da8a90483fc29b437cec297ff03d
Andrii Pylat <pilatao91@gmail.com>
2015-03-18 14:03:05 +0200

    Created footer module.

--- app/scripts/modules/BasicLayout.js
+++ app/scripts/modules/BasicLayout.js
@@ -10,16 +10,12 @@ var Partners = require('./Partners.js');
 var Schedule = require('./Schedule.jsx');
 var Registration = require('./Registration.jsx');
 var Overview = require('./Overview.jsx');
+var Footer = require('./Footer.jsx');
 
 var LayoutBasic = React.createClass({
   render: function() {
     return (
       <div className="page-wrap">
-        <footer id="footer" className="page-wrap">
-          <div className="container">
-            here to be inserted footer module
-          </div>
-        </footer>
       </div>
     );
   }
---
+++ app/scripts/modules/Footer.jsx
@@ -0,0 +1,15 @@
+'use strict';
+
+var React = require('react');
+
+var Footer = React.createClass({
+  render: function() {
+    return: (
+      <footer id="footer" className="page-wrap footer">
+        <div className="footer__container">
+          <span className="footer__info">&copy; Taras Kharuk</span>
+        </div>
+      </footer>
+    );
+  }
+});
