72dce4424024f3282c6a161d57bb2208acdbc008
Andrii Pylat <pilatao91@gmail.com>
2015-03-25 11:14:18 +0200

    improved path in config file. some style fixes.

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -2,44 +2,38 @@
 
 var config = (function() {
   //Look if we have users localisation, if not - use default.
-  var chooseLocale = function() {
-    //=======================================AVAILABLE LOCALES SEETINGS HERE
-    var availableLocales = ['en', 'uk', {defLang: 'en'}];
-    var navLng = navigator.language;
-    var userLang;
-    var setLocale;
-    var defLang;
-
-    //Check if navigator.language includes dash
-    if (navLng.indexOf('-') != -1) {
-      userLang = navLng.substr(0, navLng.indexOf('-'));
-    } else {
-      userLang = navLng;
-    }
-    //Actually set proper path
-    for (var i = 0; i < availableLocales.length; i++) {
-      if (typeof availableLocales[i] == 'object') {
-        defLang = availableLocales[i].defLang;
-      }
-      if (availableLocales[i] == userLang) {
-        setLocale = availableLocales[i];
-        break;
-      } else {
-        setLocale = defLang;
-      }
-    }
-    return setLocale;
-  };
-
   return {
-    path: {
-      schedule: 'locales/' + chooseLocale() + '/schedule.json',
-      speakers: 'locales/' + chooseLocale() + '/speakers.json',
-      partners: 'locales/' + chooseLocale() + '/partners.json',
-      mainInfo: 'locales/' + chooseLocale() + '/mainInfo.json',
-      location: 'locales/' + chooseLocale() + '/localisation.json',
-      registration: 'locales/' + chooseLocale() + '/registration.json',
-      footer: 'locales/' + chooseLocale() + '/footer.json',
+    pathJSON: function(mod) {
+      //=======================================AVAILABLE LOCALES SEETINGS HERE
+      var availableLocales = ['en', 'uk', {defLang: 'en'}];
+      var navLng = navigator.language;
+      var userLang;
+      var setLocale;
+      var defLang;
+
+      //Check if navigator.language includes dash
+      if (navLng.indexOf('-') != -1) {
+        userLang = navLng.substr(0, navLng.indexOf('-'));
+      } else {
+        userLang = navLng;
+      }
+      //Actually set proper path
+      for (var i = 0; i < availableLocales.length; i++) {
+        if (typeof availableLocales[i] == 'object') {
+          defLang = availableLocales[i].defLang;
+        }
+        if (availableLocales[i] == userLang) {
+          setLocale = availableLocales[i];
+          break;
+        } else {
+          setLocale = defLang;
+        }
+      }
+      var JSONfile = 'locales/' + setLocale + '/' + mod + '.json';
+      return JSONfile;
+    },
+
+    pathIMG: {
       conf_logo: 'images/logo.jpg'
     },
 
