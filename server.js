104f10a26ff5d8f9b435ef408d8c1cd02d669e52
Taras Kharuk <tuxuj.pes@yandex.ru>
2015-03-26 20:54:56 +0200

    add CI to server via github webhooks

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var appData = require('./package.json');
 var express = require('express');
+var Q = require('q');
 var path = require('path');
 var exec = require('child_process').exec;
 var _ = require('underscore');
@@ -34,44 +35,76 @@ app.get('*', function(req, res) {
   });
 });
 
-app.post('/payload', function(req, res){
-  var body = req.body;
-
-  if (body.commits.length){
-    console.log('GITHUB : changes to repository detected');
-    console.log('by : ', body.pusher.name);
-    pullUpdates();
-
-    function pullUpdates(){
-      exec('git pull', function(err, stdout, stderr){
-        if (!err){
-          res.status(200).send(body);
-          updateNpm();
-        }
-      });
-    };
-
-    function updateNpm(){
-      // iterate to see changes in package.json
-      var modified = _.pluck(body.commits, 'modified');
-      var isUpdateNeed;
-
-      modified.forEach(function(commit){
-        commit.forEach(function(file){
-          if (file === 'package.json'){
-            isUpdateNeed = true;
-          }
+//listen to github webhook notification
+if (env !== 'development'){
+  app.post('/payload', function(req, res) {
+    var body = req.body;
+    if (body.commits.length){
+      // console.log('GITHUB : changes to repository detected');
+      // console.log('by : ', body.pusher.email);
+      // chain of what have to be done to ensure latest steady version on test
+      // we return success status only if all steps passed
+      Q.fcall(pullUpdates, body)
+        .then(updateNpm)
+        .then(rebuildApp)
+        .then(function(){
+          res.status(200).send('Build success');
+        }, function(err){
+          res.status(409).send(err);
         });
-      });
-      if (isUpdateNeed) {
-        exec('npm install', function(err, stdout, stderr){
-          debugger;
-        });
-      }
     }
-  }
-})
+  });
+}
 
 app.listen(port);
 
 console.log(env.toUpperCase() + ' server is up and running at port : ' + port);
+// functions to be run on git push notification
+function pullUpdates(body){
+  return new Promise(function(resolve, reject){
+    exec('git pull', function(err, stdout, stderr){
+      if (err){
+        reject(err);
+      } else {
+        resolve(body);
+      }
+    });
+  });
+};
+function updateNpm(body){
+  return new Promise(function(resolve, reject){
+    var modified = _.pluck(body.commits, 'modified');
+    var isUpdateNeed;
+    // iterate to look for changes in package.json
+    modified.forEach(function(commit){
+      commit.forEach(function(file){
+        if (file === 'package.json'){
+          isUpdateNeed = true;
+        }
+      });
+    });
+    // if found - update packages
+    if (isUpdateNeed) {
+      exec('npm install', function(err, stdout, stderr){
+        if (err){
+          reject(err)
+        } else {
+          resolve(body);
+        }
+      });
+    } else {
+      resolve(body);
+    }
+  });
+}
+function rebuildApp(body){
+  return new Promise(function(resolve, reject){
+    exec('gulp build', function(err, stdout, stderr){
+      if(err){
+        reject(err);
+      } else {
+        resolve(body);
+      }
+    });
+  });
+}
