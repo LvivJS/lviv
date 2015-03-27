412fef1ce7b39030fb16a6a5e39e15ac874129db
anastasia22 <abuniak22@gmail.com>
2015-03-27 12:42:20 +0200

    basic module created

--- app/scripts/modules/Footer.jsx
+++ app/scripts/modules/Footer.jsx
@@ -7,8 +7,10 @@ var config = require('../config');
 var Footer = React.createClass({
   getInitialState: function() {
     return ({
-      data: '',
-      locales:''
+      events: [],
+      networks:{},
+      mail:'',
+      locales:{}
     });
   },
 
@@ -16,72 +18,72 @@ var Footer = React.createClass({
     utilities.ajax('get', config.path.footer, function(data) {
       var temp = JSON.parse(data);
       this.setState({
-        data: temp.data,
+        events: temp.data.events,
+        mail:temp.data.socials.email,
+        networks:temp.data.socials.networks,
         locales: temp.locales
       });
     }.bind(this));
   },
   render: function() {
-
     return (
       <footer id="footer" className="page-wrap footer">
         <div className="footer__container">
-        <Connection locales={this.state.locales} />
+          <CommunityEvents events={this.state.events} locales={this.state.locales} />
+          <Connection networks={this.state.networks} mail={this.state.mail} locales={this.state.locales} />
         </div>
       </footer>
     );
   }
 });
 
+var CommunityEvents = React.createClass({
 
-var Connection = React.createClass({
-  render:function() {
-    return(
+  render: function() {
+    var events = this.props.events.map(function(ev){
+      return(
+        <a className="footer__event" href={ev.link} key={ev.name} target="_blank">{ev.name}</a>
+      )
+    }.bind(this));
+    return (
       <div className="footer__block">
-        <h5>{this.props.locales.connection_header}</h5>
-        <div className="footer__socials">
-
-        </div>
-        <div className="footer__subscribe">
-          <form>
-             <span>{this.props.locales.subscribe_prop}</span>
-             <input type="text" placeholder="email"/>
-             <input type="button" value={this.props.locales.submit_subscribe}/>
-          </form>
-          <div className="footer__contacts">
-
-          </div>
+        <h5 className="footer__headings">{this.props.locales.events_header}</h5>
+        <div className="footer__eventLinks">
+          {events}
         </div>
       </div>
     )
   }
 });
           
-
+var Connection = React.createClass({
+  render:function() {
+    var socials= Object.keys(this.props.networks).map(function(network) {
+      var soc_class= "footer__social--" + network
+      return(
+          <a className={soc_class} href={this.props.networks[network]}
+            key={network} target="_blank"></a>
+      )
+    }.bind(this));
+    var mailto = "mailto:"+ this.props.mail;
+    return(
+      <div className="footer__block">
+        <h5 className="footer__headings">{this.props.locales.connection_header}</h5>
+        <div className="footer__socials">
+          {socials}
+        </div>
+        <div className="footer__contacts">
+          <span>{this.props.locales.contact_us}</span>
+          <a className="footer__mailLink" href={mailto}>{this.props.mail}</a>
+        </div>
+        <div className="footer__copyright">
+          <span>{this.props.locales.copyright}</span>
+        </div>
+      </div>
+    )
+  }
+});
 module.exports = Footer;
 
 
-//           <OtherEvents data={this.state.data} locales={this.state.locales} />
  
-          
-// var OtherEvents = React.createClass({
-//   getInitialState: function() {
-//     return {
-//       events:this.props.data.events
-//     }
-//   },
-//   render: function() {
-//     console.log(this.props.data.events);
-//     var events = this.state.events.map(function(ev){
-//       return(
-//         <a href={ev.link}>{ev.name}</a>
-//       )
-//     }.bind(this));
-//     return (
-//       <div className="footer__block">
-//         <h5>{this.props.locales.events_header}</h5>
-//         {events}
-//       </div>
-//     )
-//   }
-// });
