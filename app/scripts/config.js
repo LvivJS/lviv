e39e2c7ddfcb2c071ece3e786d3163f03bb54445
anastasia22 <abuniak22@gmail.com>
2015-04-02 22:34:51 +0300

    started config refactoring

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -1,51 +1,7 @@
 'use strict';
 
 var config = (function() {
-  //Look if we have users localisation, if not - use default.
-  var chooseLocale = function() {
-    //=======================================AVAILABLE LOCALES SEETINGS HERE
-    var availableLocales = ['en', 'uk', {defLang: 'en'}];
-    var navLng = navigator.language;
-    var userLang;
-    var setLocale;
-    var defLang;
-
-    //Check if navigator.language includes dash
-    if (navLng.indexOf('-') != -1) {
-      userLang = navLng.substr(0, navLng.indexOf('-'));
-    } else {
-      userLang = navLng;
-    }
-    //Actually set proper path
-    for (var i = 0; i < availableLocales.length; i++) {
-      if (typeof availableLocales[i] == 'object') {
-        defLang = availableLocales[i].defLang;
-      }
-      if (availableLocales[i] == userLang) {
-        setLocale = availableLocales[i];
-        break;
-      } else {
-        setLocale = defLang;
-      }
-    }
-    return setLocale;
-  };
-
-  return {
-    path: {
-      schedule: 'locales/' + chooseLocale() + '/schedule.json',
-      speakers: 'locales/' + chooseLocale() + '/speakers.json',
-      partners: 'locales/' + chooseLocale() + '/partners.json',
-      mainInfo: 'locales/' + chooseLocale() + '/mainInfo.json',
-      location: 'locales/' + chooseLocale() + '/location.json',
-      registration: 'locales/' + chooseLocale() + '/registration.json',
-      footer: 'locales/' + chooseLocale() + '/footer.json',
-      conf_logo: 'images/logo.jpg'
-    },
-
-    firebasePath: 'https://blistering-fire-6843.firebaseio.com/users',
-
-    modules: [
+  var modules = [
       {
         title: 'overview',
         isRendering: true,
@@ -76,7 +32,47 @@ var config = (function() {
         isRendering: true,
         order: 5
       }
-    ]
+    ];
+  //Look if we have users localisation, if not - use default.
+  var chooseLocale = function() {
+    //=======================================AVAILABLE LOCALES SEETINGS HERE
+    var availableLocales = ['en', 'uk'];
+    var defLang = 'en';
+    var navLng = navigator.language;
+    //Check if navigator.language includes dash
+    var userLang = navLng.substr(0, navLng.indexOf('-')) || navLng;
+    var setLocale;
+
+    //Actually set proper path
+    for (var i = 0; i < availableLocales.length; i++) {
+      if (availableLocales[i] == userLang) {
+        setLocale = availableLocales[i];
+        break;
+      } else {
+        setLocale = defLang;
+      }
+    }
+    return setLocale;
+  };
+  var lang = chooseLocale();
+  var path = {};
+  var pathes = modules.map(function(module) {
+    path[module.title] =  'locales/' + lang + '/' + module.title + '.json,'
+  });
+  console.log(path);
+  return {
+    path: {
+      schedule: 'locales/' + lang + '/schedule.json',
+      speakers: 'locales/' + lang + '/speakers.json',
+      partners: 'locales/' + lang + '/partners.json',
+      mainInfo: 'locales/' + lang + '/mainInfo.json',
+      location: 'locales/' + lang + '/location.json',
+      registration: 'locales/' + lang + '/registration.json',
+      footer: 'locales/' + lang + '/footer.json',
+      conf_logo: 'images/logo.jpg'
+    },
+    firebasePath: 'https://blistering-fire-6843.firebaseio.com/users',
+    modules: modules
   }
 })()
 
