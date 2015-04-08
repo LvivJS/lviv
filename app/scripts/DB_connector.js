8e5b8b3bc387b36eaa11676e5bfaf5ebb1b55bdf
Andrii Pylat <pilatao91@gmail.com>
2015-04-08 17:00:10 +0300

    created file DB_connector.js

---
+++ app/scripts/DB_connector.js
@@ -0,0 +1,19 @@
+'use strict';
+var config = require('./config');
+
+module.exports = (function() {
+  return {
+    get: function() {
+
+    },
+    write: function() {
+
+    },
+    replace: function() {
+
+    },
+    delete: function() {
+
+    }
+  };
+})();
