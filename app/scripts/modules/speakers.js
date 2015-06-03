b5b7e0706bff86c42c0b475a5f2c1785eb141bb1
Andrii Pylat <pilatao91@gmail.com>
2015-06-03 14:42:47 +0300

    #cm_42 speakers behavior is responsive, not depends on initial window size. Fixed menu header styles.

--- app/scripts/modules/speakers.js
+++ app/scripts/modules/speakers.js
@@ -3,29 +3,34 @@
 var config = require('../config');
 
 function speakers() {
+  var allSpeakers = document.getElementsByClassName('speaker');
+  var fourthSpeaker = allSpeakers[3];
+  var speakersWrap = fourthSpeaker.parentNode;
+  var isExpanded = false;
+
   //set collapsed .speakers height
-  window.onload = function() {
+  window.onload = collapseSpeakers;
+  window.onresize = collapseSpeakers;
+
+  function collapseSpeakers() {
     var viewport = document.documentElement.clientWidth;
-    var fourthSpeaker = document.getElementsByClassName('speaker')[3];
-    var speakersWrap = fourthSpeaker.parentNode;
     var fourthSpeakerTop = fourthSpeaker.offsetTop;
     var fourthSpeakerBottom = fourthSpeakerTop + fourthSpeaker.offsetHeight;
 
-    if (viewport <= config.breakPoint) {
+    if (viewport <= config.breakPoint && allSpeakers.length > 4 && !isExpanded) {
       speakersWrap.style.height = fourthSpeakerBottom - speakersWrap.offsetTop + 'px';
+    } else {
+      speakersWrap.style.height = 'auto';
     }
   };
 
   (function() {
-
     var sreakersButton = document.querySelector('.speakers__expand-button');
 
     sreakersButton.onclick = function(e) {
-      var speakersWrap = e.target.previousSibling;
-      var speakers = sreakersButton.previousSibling;
-
-      speakers.style.height = 'auto';
+      speakersWrap.style.height = 'auto';
       speakersWrap.parentNode.removeChild(sreakersButton);
+      isExpanded = true;
     };
   })();
 }
