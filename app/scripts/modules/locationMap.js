8cfbc960473081953abfc18e3b5fbbba55c79a56
Andrii Pylat <pilatao91@gmail.com>
2015-06-08 15:37:16 +0300

    #cm_75 loc-info onclick event handler has been removed for small devices.

--- app/scripts/modules/locationMap.js
+++ app/scripts/modules/locationMap.js
@@ -4,23 +4,55 @@ var config = require('../config');
 
 function locMap() {
   var viewport = window.innerWidth;
+  var menuHeight;
+  var clientHeight;
   var info = document.getElementsByClassName('loc-info')[0];
+  var infoParent = info.parentNode;
+  var hasClick = false;
 
-  if (viewport > config.breakPoint) {
-    info.onclick = function toggleInfo() {
+  var infoBlockClasses = {
+    toggleMin: function() {
       info.classList.toggle('loc-info--min');
+    },
+    removeMin: function() {
+      info.classList.remove('loc-info--min');
+    }
+  };
+  //set initial event handlers of loc-info
+  infoBlockEvents(viewport);
+
+  window.addEventListener('resize', function() {
+    viewport = window.innerWidth;
+    infoBlockEvents(viewport);
+  });
+
+  //reset info block styles when #location is not in viewport scope
+  document.addEventListener('scroll', function() {
+    clientHeight = document.documentElement.clientHeight;
+    menuHeight = document.getElementById('menu').offsetHeight;
+
+    if (viewport > config.breakPoint) {
+      if (window.pageYOffset < infoParent.offsetTop - clientHeight ||
+      window.pageYOffset >= (infoParent.offsetTop + infoParent.offsetHeight) - menuHeight) {
+        infoBlockClasses.removeMin();
+      }
+    }
+  });
+
+  //toggle position of loc-info
+  function infoBlockEvents(viewport) {
+    if ((viewport > config.breakPoint) && !hasClick) {
+      hasClick = true;
+      return info.addEventListener('click', toggleInfo, false);
+    } else if (hasClick) {
+      infoBlockClasses.removeMin();
+      hasClick = false;
+      return info.removeEventListener('click', toggleInfo, false);
     }
   }
-  //reset info block styles when #location is not in viewport scope
-  var clientHeight = document.documentElement.clientHeight;
-  var menuHeight = document.getElementById('menu').offsetHeight;
-  document.onscroll = function() {
-    var parent = document.getElementById('location');
-    var el = parent.firstChild;
-    if (window.pageYOffset < parent.offsetTop - clientHeight ||
-    window.pageYOffset >= (parent.offsetTop + parent.offsetHeight) - menuHeight) {
-      el.classList.remove('loc-info--min');
-    }
+
+  function toggleInfo() {
+    infoBlockClasses.toggleMin();
   }
 };
 
