efb5c79faef971f1e93d7d62a944fd7e7715b758
Andrii Pylat <pilatao91@gmail.com>
2015-06-12 11:51:43 +0300

    #cm_74 Wheel scroll on map is enabled only  by clicking on loc-info block - for desktop. For mobiles it is initially enabled.

--- app/scripts/modules/locationMap.js
+++ app/scripts/modules/locationMap.js
@@ -4,6 +4,7 @@ var config = require('../config');
 
 function locMap(linkText) {
   var viewport = window.innerWidth;
+  var breakPoint = config.breakPoint;
   var menuHeight;
   var clientHeight;
   var info = document.getElementsByClassName('loc-info')[0];
@@ -24,24 +25,12 @@ function locMap(linkText) {
   window.addEventListener('resize', function() {
     viewport = window.innerWidth;
     infoBlockEvents(viewport);
-  });
 
-  //reset info block styles when #location is not in viewport scope
-  document.addEventListener('scroll', function() {
-    clientHeight = document.documentElement.clientHeight;
-    menuHeight = document.getElementById('menu').offsetHeight;
-
-    if (viewport > config.breakPoint) {
-      if (window.pageYOffset < infoParent.offsetTop - clientHeight ||
-      window.pageYOffset >= (infoParent.offsetTop + infoParent.offsetHeight) - menuHeight) {
-        infoBlockClasses.removeMin();
-      }
-    }
   });
 
   //toggle position of loc-info
   function infoBlockEvents(viewport) {
-    if ((viewport > config.breakPoint) && !hasClick) {
+    if ((viewport > breakPoint) && !hasClick) {
       hasClick = true;
       return info.addEventListener('click', toggleInfo, false);
     } else if (hasClick) {
@@ -63,17 +52,59 @@ function locMap(linkText) {
     var mapProp = {
       center: myLatlng,
       zoom: 17,
+      scrollwheel: true,
       mapTypeId: google.maps.MapTypeId.ROADMAP,
-      draggable: false
+      draggable: true
     };
+    var isWhellScroll;
+    var divHref = 'http://maps.google.com/maps?&z=' + mapProp.zoom + '&q=' + lat + ',' + lng;
     var map = new google.maps.Map(document.getElementById('googleMap'), mapProp);
     var marker = new google.maps.Marker({
       position: myLatlng,
       map: map,
       title: 'Awesome place for conference! See you there!'
     });
-    //this will be used from control div
-    var divHref = 'http://maps.google.com/maps?&z=' + mapProp.zoom + '&q=' + lat + ',' + lng;
+
+    window.addEventListener('resize', addWheelZoom);
+
+    //reset info block styles when #location is not in viewport scope
+    document.addEventListener('scroll', function() {
+      clientHeight = document.documentElement.clientHeight;
+      menuHeight = document.getElementById('menu').offsetHeight;
+
+      if (viewport > breakPoint) {
+        if (window.pageYOffset < infoParent.offsetTop - clientHeight ||
+        window.pageYOffset >= (infoParent.offsetTop + infoParent.offsetHeight) - menuHeight) {
+          infoBlockClasses.removeMin();
+          addWheelZoom();
+        }
+      }
+    });
+
+    //handle wheel zoom
+    function addWheelZoom() {
+      var locinfo = document.getElementsByClassName('loc-info')[0];
+      if (viewport > breakPoint) {
+        isWhellScroll = false;
+        locinfo.addEventListener('click', handleWhellZoom);
+      } else {
+        isWhellScroll = true;
+        locinfo.removeEventListener('click', handleWhellZoom);
+      }
+      return map.setOptions({'scrollwheel': isWhellScroll, 'draggable': isWhellScroll});
+    }
+    addWheelZoom();
+
+    function handleWhellZoom() {
+      if (!isWhellScroll) {
+        isWhellScroll = true;
+        map.setOptions({'scrollwheel': isWhellScroll, 'draggable': isWhellScroll});
+      } else {
+        isWhellScroll = false;
+        map.setOptions({'scrollwheel': isWhellScroll, 'draggable': isWhellScroll});
+      }
+    }
+
     return addControlDiv(divHref);
   }
 
