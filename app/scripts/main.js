d50abaf1f6b2123a6214b10a54452f64370d37e4
Taras Kharuk <tuxuj.pes@yandex.ru>
2015-06-12 21:53:16 +0300

    [hotfix] fix capital letter name for unix env

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var menu = require('./modules/menu.js');
-var locationMap = require('./modules/locationmap.js');
+var locationMap = require('./modules/locationMap.js');
 var registration = require('./modules/registration.js');
 var schedule = require('./modules/schedule.js');
 var speakers = require('./modules/speakers.js');
