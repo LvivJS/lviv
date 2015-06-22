6b689024eb5caa4031e7ee332b35ba669e85cf65
Taras Kharuk <tuxuj.pes@yandex.ru>
2015-06-22 16:06:11 +0300

    build statics, tune server

--- server.js
+++ server.js
@@ -9,8 +9,9 @@ var ci = require('./simpleCI');
 var fs = require('fs');
 var MobileDetect = require('mobile-detect');
 
-var port = process.env.port || 8080;
+var port = process.env.port || 3000;
 var env = process.env.NODE_ENV || 'development';
+var staticDir = env == 'development' ? 'dist' : 'static';
 // utilities
 app.locals.moment = require('moment');
 var data = {};
@@ -38,27 +39,12 @@ data.config.modules.forEach(function(module) {
 app.use(compress());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, staticDir)));
 // listens to payloads from github
-app.use('/github-payload', ci);
+// app.use('/github-payload', ci);
 
 app.set('view engine', 'jade');
 app.set('views', path.join(__dirname, 'views'));
-
-//register bad bots
-app.get('/nolink', function(req, res, next) {
-  var usrAgnt = req.get('User-Agent');
-
-  var blackList = require('./dist/locales/badBots.json');
-  blackList.push({name: usrAgnt});
-  fs.writeFile('./dist/locales/badBots.json', JSON.stringify(blackList), function (err) {
-    if (err) {
-      throw err;
-    }
-    console.log('Bad bot list has been updated!');
-  });
-  checkUserAgent(req, res, function() {});
-});
 
 // index url
 app.get('/', function(req, res) {
@@ -67,23 +53,36 @@ app.get('/', function(req, res) {
   });
 });
 
+//register bad bots
+// app.get('/nolink', function(req, res, next) {
+//   var usrAgnt = req.get('User-Agent');
+// 
+//   var blackList = require('./dist/locales/badBots.json');
+//   blackList.push({name: usrAgnt});
+//   fs.writeFile('./dist/locales/badBots.json', JSON.stringify(blackList), function (err) {
+//     if (err) {
+//       throw err;
+//     }
+//     console.log('Bad bot list has been updated!');
+//   });
+//   checkUserAgent(req, res, function() {});
+// });
+
 //handle data from form
 // todo: should be POST to "/users"
-app.post('/', function(req, res, next) {
-  files.push('users', req.body);
-  res.status(200).send('Thanks for registration!');
-});
-
-//Route not found -- Set 404
-app.get('*', function(req, res) {
-  checkUserAgent(req, res, function() {
-    return res.status(404).send('Sorry this page does not exist!');
-  });
-});
-
+// app.post('/', function(req, res, next) {
+//   files.push('users', req.body);
+//   res.status(200).send('Thanks for registration!');
+// });
+// 
+// //Route not found -- Set 404
+// app.get('*', function(req, res) {
+//   checkUserAgent(req, res, function() {
+//     return res.status(404).send('Sorry this page does not exist!');
+//   });
+// });
 
 app.listen(port);
-
 console.log(env.toUpperCase() + ' server is up and running at port : ' + port);
 
 function checkUserAgent(req, res, next) {
