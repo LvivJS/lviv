c942bc2f4a11dc57a8d45ceae12e5e4af2b4fa2b
Andrii Pylat <pilatao91@gmail.com>
2015-06-24 14:36:05 +0300

    #lvjs_5 Created registration button which refers to page provided by "Ticket for event" service.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,13 +2,11 @@
 
 var menu = require('./modules/menu.js');
 var locationMap = require('./modules/locationMap.js');
-var registration = require('./modules/registration.js');
 var schedule = require('./modules/schedule.js');
 var speakers = require('./modules/speakers.js');
 
 //TODO: somehow disable locatinMap for mobiles
 menu();
 locationMap('View on Google Maps');
-registration();
 schedule();
 speakers('See all speakers');
